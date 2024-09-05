b6a1696f4704f10c9ad25a187334926251715844
YoungGoblin <1bainsish2@hdsb.ca>
2024-09-05 18:47:06 +0000

    Hello World!!

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,4 @@
 // Log "Hello World" to the console.
-// Log four additional messages of your choosing. 
-// Comment each message appropriately. 
-console.log("Testing!");
-console.log("This is a change!");
+// Log four additional messages of your choosing.
+// Comment each message appropriately.
+console.log("Hello World");
